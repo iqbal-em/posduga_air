7f6a8a8ad5b11842671ab4bf0d9bd1fdc22eb902
Iqbal Nurfadhillah <iqbalnfad@gmail.com>
2021-09-19 14:48:44 +0700

    fix8_change_durasi_pengiriman

--- fix8.py
+++ fix8.py
@@ -23,13 +23,14 @@ ketinggian_air = 0
 last_kalibrasi = 0
 response2 = os.system("sudo -S pigpiod")
 
+'''
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.OUT) #pin Relay Modem
 GPIO.output(4, GPIO.LOW)#kondisi mati
 
 GPIO.setup(17, GPIO.OUT) #pin Relay Kamera
 GPIO.output(17, GPIO.LOW)#kondisi mati
-
+'''
 tinggi_sensor = 752
 
 #SERIAL_PORT = "/dev/ttyAMA0"  # Raspberry Pi 3
@@ -39,10 +40,10 @@ siaga1 = 201
 siaga2 = 150
 siaga3 = 100
 set_millis = 0
-lvl_siaga1 = 300000
-lvl_siaga2 = 300000
-lvl_siaga3 = 300000
-lvl_siaga4 = 300000
+lvl_siaga1 = 1800000
+lvl_siaga2 = 3600000
+lvl_siaga3 = 10800000
+lvl_siaga4 = 21600000
 status = ""
 flag_status = 0
 last_flag_status = 0
@@ -51,6 +52,8 @@ lastupdate_jam = ""
 url = "https://posduga.sysable.io/api/sendjsondata"
 url1 = "https://posduga.sysable.io/api/api-device-by-imei/088298203821"
 jadwal_pengiriman = ""
+current_time = ""
+date = ""
 
 
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
@@ -143,13 +146,13 @@ def convertToBinaryData(filename):
         binaryData = file.read()
     return binaryData
 
-def kirim_data(data,img):
+def kirim_data(data,img, waktu, tanggal):
     
     if (check_ping() == 0):
         img = "data:image/png;base64," + str(img)
     else :
         img = " "
-    data_fix = {"foto_cam":img,"ketinggian_air":data,"imei":imei}
+    data_fix = {"foto_cam":img,"ketinggian_air":data,"imei":imei, "waktu":waktu, "tanggal":tanggal }
     try:
         r = requests.post(url, data=json.dumps(data_fix), headers=headers)
         r.close()
@@ -158,7 +161,7 @@ def kirim_data(data,img):
         get_data_durasi()
 
 def get_data_durasi():
-    global siaga1, siaga2, siaga3, siaga4, lvl_siaga1, lvl_siaga2, lvl_siaga3, lvl_siaga4, last_updatetanggal
+    global siaga1, siaga2, siaga3, siaga4, lvl_siaga1, lvl_siaga2, lvl_siaga3, lvl_siaga4, jadwal_pengiriman
     try : 
         r =  requests.get(url=url1)
         data = r.json()
@@ -171,8 +174,9 @@ def get_data_durasi():
         lvl_siaga4 = (data['data'][0]['siaga']['durasi_siaga_4'])*1000
         siaga3 = data['data'][0]['siaga']['min_siaga_3']
         lastupdate = data['last_update']
-        lastupdate_jam = lastupdate[11:19]
-        print("lastupdate_jam :" +lastupdate_jam)
+        jadwal_pengiriman = lastupdate[11:19]
+        print("Jadwal Pengiriman :" + jadwal_pengiriman)
+        #jadwal_pengiriman = (data['data'][0]['siaga']['updated_at'])
         kirim_data_full()
     except requests.exceptions.ConnectionError:
         
@@ -188,33 +192,35 @@ def kirim_data_full():
     #os.system('sudo route add 27.131.0.10 gw 192.168.100.1')
     if (check_ping() == 0):
         time.sleep(2)
-      cam = Client('http://192.168.1.64', 'admin', 't4ng3r4ng')
-      print("starting picture capture")
-      vid = cam.Streaming.channels[102].picture(method ='get', type = 'opaque_data')
-      bytes = b''
-      with open('img.png', 'wb') as f:
-          for chunk in vid.iter_content(chunk_size=1024):
-              bytes += chunk
-              a = bytes.find(b'\xff\xd8')
-              b = bytes.find(b'\xff\xd9')
-              if a != -1 and b != -1:
-                  jpg = bytes[a:b+2]
-                  bytes = bytes[b+2:]
-                  i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
-                  cv2.imwrite('img.png', i)
-                  #cv2.imshow('i', i)
+        cam = Client('http://192.168.1.64', 'admin', 't4ng3r4ng')
+        print("starting picture capture")
+        vid = cam.Streaming.channels[102].picture(method ='get', type = 'opaque_data')
+        bytes = b''
+        with open('img.png', 'wb') as f:
+            for chunk in vid.iter_content(chunk_size=1024):
+                bytes += chunk
+                a = bytes.find(b'\xff\xd8')
+                b = bytes.find(b'\xff\xd9')
+                if a != -1 and b != -1:
+                    jpg = bytes[a:b+2]
+                    bytes = bytes[b+2:]
+                    i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+                    cv2.imwrite('img.png', i)
+                    #cv2.imshow('i', i)
     else :
       print("CCTV NOT DETECTED")
-        
+
+    
+
     hostname = "posduga.sysable.io"
     if(check_url(hostname) == 0 or check_url(hostname) == 512):
       buffer_img = compress_img('img.png')
       if(check_ping()) == 0 :
-          response = kirim_data(int(ketinggian_air_fix),buffer_img)
+          response = kirim_data(ketinggian_air_fix,buffer_img,current_time, date)
           print(response)
       else :
           dump = " "
-          response = kirim_data(int(ketinggian_air_fix),buffer_img)
+          response = kirim_data(ketinggian_air_fix,buffer_img,current_time, date)
           print(response)
     else :
         print("No Internet")
@@ -226,16 +232,14 @@ def kirim_data_full():
     #GPIO.output(17, GPIO.LOW)#Kamera Mati   
     
 def main():
-   global set_millis,status, ketinggian_air, ketinggian_air_fix, last_ketinggian_air, tinggi_sensor, flag_status, last_flag_status, last_kalibrasi
+   global set_millis,status, ketinggian_air, ketinggian_air_fix, last_ketinggian_air, tinggi_sensor, flag_status, last_flag_status, last_kalibrasi, current_time, date
    pwm_millis = round(int(time.time() * 1000))
-   camera_millis = round(int(time.time() * 1000))
-   data_millis = round(int(time.time() * 1000))
    print("Initiate Kalibrasi Sensor ......")
    pi = pigpio.pi()
    for i in range(10):
        p1 = PWM_read(pi, 12)
        time.sleep(1)
-       if (i == 1):
+       if (i == 0):
            last_kalibrasi = ketinggian_air
        if ((ketinggian_air < last_kalibrasi) and (last_kalibrasi != 0)):
            last_ketinggian_air = ketinggian_air
@@ -247,25 +251,60 @@ def main():
        print("Update Data")
        get_data_durasi()
    while True :
+       current_millis = round(int(time.time() * 1000))
        t = time.localtime()
        current_time = time.strftime("%H:%M:%S", t)
        print("Current Time : " + current_time)
-       print("jadwal Pengiriman:"+ jadwal_pengiriman)
-       if (current_time == jadwal_pengiriman):
-           p1 = PWM_read(pi, 12)
-           time.sleep(1)
-           #print("Real Ketinggian_air :", int(ketinggian_air))
-           # #filter noise sensor
-           p1.cancel()
-           if(ketinggian_air == 10000):
-               ketinggian_air_fix = last_ketinggian_air
-           else:
-               if(abs(int(ketinggian_air) - last_ketinggian_air))>40 and last_ketinggian_air != 0 and int(ketinggian_air) != 0:
-                   ketinggian_air_fix = last_ketinggian_air
-                   print("filter noise")
-               else :
-                   ketinggian_air_fix = int(ketinggian_air) 
-           last_ketinggian_air = ketinggian_air_fix 
+       date = datetime.datetime.now().date()
+
+       if (current_millis - pwm_millis) > 10000 :
+          p1 = PWM_read(pi, 12)
+          time.sleep(1)
+
+          #print("Real Ketinggian_air :", int(ketinggian_air))
+          #filter noise sensor
+          if(abs(ketinggian_air - last_ketinggian_air)>50 and last_ketinggian_air != 0 and ketinggian_air != 0):
+              ketinggian_air_fix = last_ketinggian_air
+              print("filter noise")
+          else :
+              ketinggian_air_fix = ketinggian_air
+              last_ketinggian_air = ketinggian_air
+          pwm_millis = current_millis
+             
+          print("Ketinggian_air :", ketinggian_air_fix)
+          #print("Last_ketinggian:",int(last_ketinggian_air))
+
+          p1.cancel()
+          
+          if(int(ketinggian_air_fix) > siaga1):
+              flag_status = 1
+              set_millis = lvl_siaga1
+              status =  "siaga1"
+          elif(int(ketinggian_air_fix) > siaga2):
+              flag_status = 2
+              set_millis = lvl_siaga2
+              status =  "siaga2"
+          elif(int(ketinggian_air_fix) > siaga3):
+              flag_status = 3
+              set_millis = lvl_siaga3
+              status =  "siaga3"
+          else:
+              flag_status = 4
+              set_millis = lvl_siaga4
+              status =  "siaga4"
+              print("Durasi Pengiriman : " ,(set_millis/(1000*60)) ," menit")
+              print("Status : ", status)
+              print("flag_status: ", flag_status)
+    
+          if (flag_status != last_flag_status and last_ketinggian_air != 0 and last_flag_status !=0):
+              camera_millis = current_millis
+              print("perubahan status")
+              kirim_data_full()
+              last_flag_status = flag_status    
+              last_ketinggian_air = ketinggian_air_fix 
+            
+    
+       if (current_time == lastupdate_jam):
            kirim_data_full()
    
          
