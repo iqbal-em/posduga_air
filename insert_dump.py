302bd19dddf1c3e118d94482b5698e811dce4eca
Iqbal Nurfadhillah <iqbalnfad@gmail.com>
2021-10-07 11:11:13 +0700

    fix9_testing_img

---
+++ insert_dump.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import datetime
+import MySQLdb
+import time
+
+
+def kirim_data_local(tanggal, waktu, ketinggian_air, img, status, next_jadwal) :
+    db = MySQLdb.connect("localhost", "admin", "t4ng3r4ng", "posduga_air")
+    curs=db.cursor() 
+    #kirim data lokal
+
+    #
+    
+    temp_waktu = str(date) +  str(current_time)
+    img = "data:image/png;base64," + str(img)
+    
+    #tmp_img = 'home/pi/posduga_air/img/%s',temp_waktu
+    curs.execute ("""INSERT INTO data(ketinggian_air,data_cam,waktu,tanggal,status,next_jadwal)
+            values(%s, %s,%s,%s,%s,%s)""",(ketinggian_air, img, waktu, tanggal, status, next_jadwal))
+
+    db.commit()
+    print ("kirim data local",db)
+
+t = time.localtime()
+current_time = time.strftime("%H:%M:%S", t)
+       
+date = datetime.datetime.now().date()
+
+kirim_data_local()
