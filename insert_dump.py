25663d0358a8c1628966de28dcb825fc03e878ac
Iqbal Nurfadhillah <iqbalnfad@gmail.com>
2021-10-11 15:37:41 +0700

    fix_bug

--- insert_dump.py
+++ insert_dump.py
@@ -5,15 +5,22 @@ import MySQLdb
 import time
 
 
-def kirim_data_local(tanggal, waktu, ketinggian_air, img, status, next_jadwal) :
+def kirim_data_local() :
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+       
+    date = datetime.datetime.now().date()
     db = MySQLdb.connect("localhost", "admin", "t4ng3r4ng", "posduga_air")
     curs=db.cursor() 
     #kirim data lokal
 
     #
-    
+    ketinggian_air = 200
+    tanggal =
     temp_waktu = str(date) +  str(current_time)
-    img = "data:image/png;base64," + str(img)
+    img = "data:image/png;base64," 
+    status = 0
+    next_jadwal = 
     
     #tmp_img = 'home/pi/posduga_air/img/%s',temp_waktu
     curs.execute ("""INSERT INTO data(ketinggian_air,data_cam,waktu,tanggal,status,next_jadwal)
@@ -22,9 +29,6 @@ def kirim_data_local(tanggal, waktu, ketinggian_air, img, status, next_jadwal) :
     db.commit()
     print ("kirim data local",db)
 
-t = time.localtime()
-current_time = time.strftime("%H:%M:%S", t)
-       
-date = datetime.datetime.now().date()
+
 
 kirim_data_local()
