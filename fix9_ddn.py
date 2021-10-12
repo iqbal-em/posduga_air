63cff70065eea6e65c6480ccfedc7603f6ca6ffe
Iqbal Nurfadhillah <iqbalnfad@gmail.com>
2021-10-12 17:23:07 +0700

    fix9_ddn

--- fix9_ddn.py
+++ fix9_ddn.py
@@ -516,7 +516,7 @@ def main():
            elapsed = timedelta(minutes=5)
            print("elapsed :",elapsed)
        #print("elapsed :",elapsed)
-       if (str(current_time) == jadwal_pengiriman or (elapsed < timedelta(minutes=2) and flag_data_kirim == 1 and elapsed > 60)):
+       if (str(current_time) == jadwal_pengiriman or (elapsed < timedelta(minutes=2) and flag_data_kirim == 1 and elapsed.minute > 30)):
            flag_data_kirim = 0
            if (flag_kirim == 0):
                flag_kirim = 1
