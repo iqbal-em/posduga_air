022bc9ef71a5f927ecb97cd6863b342026a01ed5
Iqbal Nurfadhillah <iqbalnfad@gmail.com>
2021-10-13 10:54:32 +0700

    fix9_dummy

--- fix9_dummy_local_ngirim.py
+++ fix9_dummy_local_ngirim.py
@@ -321,7 +321,7 @@ def kirim_data_full():
     
     hostname = "posduga.sysable.io"
     print("cek url db local", check_url(hostname))
-    if(check_url(hostname) == 0  ):
+    if(check_url(hostname) == 512  ):
       if(check_ping()) == 0 :
           buffer_img = compress_img('img.png')
           #print("waktu :" + converter_json(current_time))
@@ -336,7 +336,7 @@ def kirim_data_full():
       jadwal_pengiriman = response
       status1 = 0
     
-    elif(check_url(hostname) == 512) :
+    elif(check_url(hostname) == 0) :
         status1  = 1
         if flag_status == 0 :
             jadwal_pengiriman = waktu_pengiriman
