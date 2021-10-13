7e5e5d3a895022068a6449338e1411086eb51108
Iqbal Nurfadhillah <iqbalnfad@gmail.com>
2021-10-13 11:11:09 +0700

    fix9_dummy

--- fix9_dummy_local_ngirim.py
+++ fix9_dummy_local_ngirim.py
@@ -255,14 +255,13 @@ def cek_data_local() :
     data_dict = {}
     print("ubah data", db)
     temp_data_local = curs.fetchall()
-    print(temp_data_local)
-    
-    ''''for x in temp_data_local:
-        data_fix = {"foto_cam":x(2),"ketinggian_air":x(1),"imei":imei, "waktu":x(3), "tanggal":x(4) }
+
+    for x in temp_data_local:
+        data_fix = {"foto_cam":x[2]),"ketinggian_air":x[1],"imei":imei, "waktu":x[3]), "tanggal":x[4]) }
         kirim_data_local_server(data_fix)
         ubah_data_local(x(0))
-    '''
     
+
 def ambil_data_local_terakhir() :
     db = MySQLdb.connect("localhost", "admin", "t4ng3r4ng", "posduga_air")
     curs=db.cursor()
