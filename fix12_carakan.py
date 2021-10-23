c415558205826e7b527b836a62b8f3167655c6d4
Iqbal Nurfadhillah <iqbalnfad@gmail.com>
2021-10-23 14:08:48 +0700

    fix12

--- fix12_carakan.py
+++ fix12_carakan.py
@@ -164,7 +164,7 @@ def kirim_data(data,img, waktu, tanggal):
         print(r)
         data = r.__dict__['_content'] #pengambilan data jadwal selanjutnya
         print(data)
-        data = json.loads(data)
+        #data = json.loads(data)
        
         #jadwal_pengiriman = jadwal_pengiriman[11:len(jadwal_pengiriman)] #pengambilan data next_schedulu di dict jadwal pengiriman
         #status = str(data['status']) 
