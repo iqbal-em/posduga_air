5195ca21c74836c09b50895bd23da45eaabb6b89
Iqbal Nurfadhillah <iqbalnfad@gmail.com>
2021-10-25 15:51:19 +0700

    cron

--- cron.py
+++ cron.py
@@ -5,7 +5,7 @@ def delete_data_db_local_weekly():
     curs=db.cursor()
     query = """DELETE FROM data WHERE tanggal < ADDDATE(NOW(),-7)"""
     curs.execute(query)
-    db.commit()
-    print(curs)
+    print(db.commit())
+    
 
 delete_data_db_local_weekly()
