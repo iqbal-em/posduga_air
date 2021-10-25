9af171fa8533fe42040ee5ee8d8dffa2730f5241
Iqbal Nurfadhillah <iqbalnfad@gmail.com>
2021-10-25 15:56:08 +0700

    fix_cron

--- cron.py
+++ cron.py
@@ -3,7 +3,7 @@ import MySQLdb
 def delete_data_db_local_weekly():
     db = MySQLdb.connect("localhost", "admin", "t4ng3r4ng", "posduga_air")
     curs=db.cursor()
-    query = """DELETE FROM data WHERE tanggal < ADDDATE(NOW(),-7)"""
+    query = """DELETE FROM data WHERE tanggal < ADDDATE(NOW(),-6)"""
     curs.execute(query)
     db.commit()
     tes = curs.fetchall()
